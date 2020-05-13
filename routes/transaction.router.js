c51f69cadc5eb177d32ad14b3662b9fed414d3ce
Glitch (hello-express) <none>
2020-05-13 23:55:46 +0000

    ⛰🎠 Checkpoint ./views/transactions.pug:15968247/2552 ./routes/transaction.router.js:15968247/282

--- routes/transaction.router.js
+++ routes/transaction.router.js
@@ -5,7 +5,9 @@ const shortid= require("shortid");
 const db = require("../db");
 
 router.get("/", (req, res) => {
-  res.render("transactions",{transactions:db.get("transactions").value()});
+  res.render("transactions",{users: db.get("users").value(),
+                             books: db.get("books").value(),
+                             transactions:db.get("transactions").value()});
 });
 
 module.exports= router;
