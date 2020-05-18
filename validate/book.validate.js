e946c9a37c8b39ce66ec2042a57ab232818a56d8
Glitch (mellow-verbena-impatiens) <none>
2020-05-18 14:25:03 +0000

    🏩🍽 Checkpoint ./validate/book.validate.js:15968247/514 ./db.json:15968247/1479

---
+++ validate/book.validate.js
@@ -0,0 +1,17 @@
+var db =  require("../db");
+
+module.exports.postBook = function (req, res, next) {
+  var errors = [];
+  if(!req.body.title){
+    errors.push('Title is required...')
+  }
+  if(!req.body.description){
+    errors.push('Description is require...')  
+  } 
+  if (errors.length){
+    res.render('books', {books:db.get('books').value(), errors});
+    return ;
+  }
+  
+  
+}
