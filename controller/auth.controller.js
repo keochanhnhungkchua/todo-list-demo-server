5e7257b0018ad262d9f3d6fbded0a9f935264358
Glitch (mellow-verbena-impatiens) <none>
2020-05-19 08:41:31 +0000

    🚘⏲ Checkpoint ./db.json:15968247/3077 ./controller/auth.controller.js:15968247/302

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -3,3 +3,17 @@ var db = require("../db");
 module.exports.login = (req, res) => {
   res.render("login");
 };
+
+module.exports.postLogin = (req, res) =>{
+ var email = req.body.email;
+ var password = req.body.password;
+ var user = db.get("user")
+              .find({email:email})
+              .value();
+  
+ if (!user){
+    res.render("login");
+    return;
+ } 
+
+}
