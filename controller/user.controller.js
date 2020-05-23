14e7470674db46d8262f53b73588736dbb6a07a5
Glitch (mellow-verbena-impatiens) <none>
2020-05-23 15:05:28 +0000

    📼⚡️ Checkpoint ./controller/user.controller.js:15968247/37 ./db.json:15968247/7 ./views/users.pug:15968247/52

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -29,7 +29,7 @@ module.exports.editUser = (req, res) => {
 //post
 module.exports.postCreateUser = (req, res) =>{ 
   req.body.id=shortid.generate();
-  req.body.avatar = req.file.path
+  req.body.avatar = req.file.path.split("/").slice(1).join('/');
   db.get("users")
     .push(req.body)
     .write();
