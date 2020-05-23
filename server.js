10a479bf5bf7ac4d2813bba04d740e0735d3e9e0
Glitch (mellow-verbena-impatiens) <none>
2020-05-23 02:01:31 +0000

    👩‍❤️‍💋‍👩✋ Checkpoint ./package.json:15968247/3324 ./server.js:15968247/226

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-// server.js
-// where your node app starts
+require('dotenv').config();
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
