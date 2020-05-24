2a1c10d7c3e0e51890eca779e6b6f04e03f49922
Glitch (mellow-verbena-impatiens) <none>
2020-05-24 02:16:32 +0000

    🛋🎑 Checkpoint ./package.json:15968247/1799 ./controller/user.controller.js:15968247/583 ./routes/user.router.js:15968247/9

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,14 @@
 const shortid= require("shortid");
+var cloudinary = require('cloudinary').v2
+
 const db = require("../db");
 var cookies=0;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 module.exports.index = (req, res) => {
   res.render("users",{users:db.get("users").value()});
