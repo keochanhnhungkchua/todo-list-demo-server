f10929c04e97d8687b47074fe2e7a1a513ce8d7c
Glitch (mellow-verbena-impatiens) <none>
2020-05-25 15:09:45 +0000

    ❄️🌆 Checkpoint ./views/index.pug:15968247/38 ./middleware/session.middleware.js:15968247/3941 ./routes/cart.router.js:15968247/184 ./controller/cart.controller.js:15968247/660

--- middleware/session.middleware.js
+++ middleware/session.middleware.js
@@ -1,40 +1,42 @@
 var shortid = require("shortid");
-var db = require('../db')
+var db = require("../db");
 
 module.exports = function(req, res, next) {
   var sessionId = req.signedCookies.sessionId;
-  if(!sessionId){
+  if (!sessionId) {
     var sessionId = shortid.generate();
-    res.cookie("sessionId", sessionId ,{ 
-      signed: true});
+    res.cookie("sessionId", sessionId, {
+      signed: true
+    });
     db.get("sessions")
-      .push({id: sessionId})
-      .write();  
-  } 
-  var data = db.get('sessions').value();
-     data.map(item => {
-       if(item.id !== sessionId){
-         db.get('sessions')
-           .remove({id : item.id})
-           .write();
-       }
-     });
-  //count number boook in cart
-  var session = db.get('sessions')
-                  .find({id : sessionId})
-                  .value();
-  //console.log(session);
-  //get values of ojbect and sum => show (set locals for "quantity" show(index>cart)
- res.locals.quantity = Object.values(session.cart)
-                              .reduce( (a , b) => a + b);
-  var items = session.cart;
-  var books = Object.keys(session.cart).map( key => {
-   var book = db.get("books")
-                .find({id : key})
-                .value();
-       book.quantity = items[key];
-    return book
+      .push({ id: sessionId })
+      .write();
+  }
+  var data = db.get("sessions").value();
+  data.map(item => {
+    if (item.id !== sessionId) {
+      db.get("sessions")
+        .remove({ id: item.id })
+        .write();
+    }
   });
-  res.locals.books=books;
+  //count number boook in cart
+  var session = db
+    .get("sessions")
+    .find({ id: sessionId })
+    .value();
+  //get values of ojbect and sum => show (set locals for "quantity" show(index>cart)
+  var items = session.cart;
+  res.locals.quantity = Object.values(items).reduce((a, b) => a + b);
+//get data for cart page
+  var books = Object.keys(items).map(key => {
+    var book = db //get data form books with id form session input by user
+      .get("books")
+      .find({ id: key })
+      .value();
+    book.quantity = items[key]; //insert quantity 
+    return book;
+  });
+  res.locals.books = books;
   next();
-}
+};
