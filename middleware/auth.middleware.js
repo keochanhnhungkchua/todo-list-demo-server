9d7262ace2e391fb2f3ffe2014725d6b3e2ea420
Glitch (mellow-verbena-impatiens) <none>
2020-05-25 15:21:44 +0000

    🏁💪 Checkpoint ./middleware/auth.middleware.js:15968247/222 ./server.js:15968247/23 ./views/index.pug:15968247/104

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -12,5 +12,15 @@ module.exports.requireAuth = (req, res, next) => {
     return ;
   }
   res.locals.user = user;
+  res.locals.isAdmin = user.isAdmin;
   next();
 }
+
+module.exports.isAdmin = function(req, res, next) {
+  if (!res.locals.isAdmin) {
+    //res.send("NO PERMISSION!");
+    res.redirect('/books');
+    return;
+  }
+  next();
+};
