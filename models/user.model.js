583327f2969cedeebc14b7561f08ca995bea8d46
Glitch (mellow-verbena-impatiens) <none>
2020-06-01 15:44:12 +0000

    🍱🎐 Checkpoint ./controller/auth.controller.js:15968247/6 ./models/user.model.js:15968247/139

--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,10 @@ var userSchema = new mongoose.Schema({
   name : String,
   email: String,
   password: String,
-  avtar : String,
+  avtar : {
+    type :String,
+    default:"https://res.cloudinary.com/keochanhnhungkchua/image/upload/v1590285910/ocuhhvnjaflr8ceppo2s.png"
+  },
   wrongLoginCount:{
     type : Number,
     default: 0
