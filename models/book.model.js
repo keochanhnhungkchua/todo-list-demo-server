ef06bbf28ba52129591a18dffdcae29cffe70095
Glitch (mellow-verbena-impatiens) <none>
2020-06-01 14:32:12 +0000

    🙆🌪 Checkpoint ./models/book.model.js:15968247/2385 ./middleware/auth.middleware.js:15968247/6

--- models/book.model.js
+++ models/book.model.js
@@ -1,12 +1,1 @@
-var mongoose = require('mongoose');
-
-var bookSchema = new mongoose.Schema({
-  "title": String,
-  "description": String,
-  "image": {
-      type: String,
-      default:
-        "https://responsiblefinance.org.uk/wp-content/uploads/2015/08/aba-04.png"
-      }
-});
-module.exposts = mongoose.model(bookSchema);
+var mongoose = 
