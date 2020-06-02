297c23fc5a33ae1ed7d17d0c47efc4915a1fc002
Glitch (spectacled-grape-icecream) <none>
2020-06-02 12:21:03 +0000

    Import of ngoba/demo-user-mongodb-use-mongoose

--- models/transaction.model.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-var transactionSchema = new mongoose.Schema({
-  userId : String,
-  booksId: Object
-});
-
-module.exports = mongoose.model("Transaction", transactionSchema);
