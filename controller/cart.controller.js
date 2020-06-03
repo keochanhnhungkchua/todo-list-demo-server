65d0f931d686d039ce50967f5b2ed7ef6dddf9d2
Glitch (spectacled-grape-icecream) <none>
2020-06-03 07:14:54 +0000

    💪💵 Checkpoint ./controller/cart.controller.js:15968247/165

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -1,17 +1,21 @@
 const shortid= require("shortid");
 const db = require("../db");
 
+var Session = require("../models/session.model");
 
 module.exports.index = function(req, res) {
   res.render("cart");
 };
-module.exports.addToCart = (req, res ) => {
+
+module.exports.addToCart =async (req, res ) => {
   var bookId = req.params.bookId;
   var sessionId = req.signedCookies.sessionId;
-  
+  var session = await Session.findOne({sessionId : sessionId});
   if(!sessionId){
     res.redirect('/books');
     return;
+  }else{
+    
   }
   // var count = db.get('sessions')
   //               .find({ id : sessionId})
