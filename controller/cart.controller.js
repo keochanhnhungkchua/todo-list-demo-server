d27c0f7ba1a41ffe08c16aacb5c550c3c6c862c8
Glitch (spectacled-grape-icecream) <none>
2020-06-04 03:33:15 +0000

    🏛🏓 Checkpoint ./controller/cart.controller.js:15968247/6500

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -3,7 +3,7 @@ const db = require("../db");
 
 var Session = require("../models/session.model");
 var Book = require("../models/book.model");
-var Transaction = require("../models/transaction.model")
+var Transaction = require("../models/transaction.model");
 
 module.exports.index = async function(req, res) {
   var sessionId = req.signedCookies.sessionId;
@@ -56,24 +56,24 @@ module.exports.hire = async (req, res) => {
   //   .get("sessions")
   //   .find({ id: sessionId })
   //   .value();
-  var data = await Session.findOne({sessionId : sessionId})
+  var data = await Session.findOne({ sessionId: sessionId });
   // var transactionId = db
   //   .get("transactions")
   //   .find({ userId: user.id })
   //   .value();
-var transactionId =await Transaction.findOne({userId : user.id})
+  var transactionId = await Transaction.findOne({ userId: user.id });
   if (!transactionId) {
     var item = {
-      booksId :data.cart,
-      userId : user.id
-    }
-     await Transaction.create(item, function(err, small) {
+      booksId: data.cart,
+      userId: user.id
+    };
+    await Transaction.create(item, function(err, small) {
       if (err) return console.log(err);
     });
     await Session.findOneAndUpdate({ sessionId }, { cart: {} });
     console.log(data.cart);
     console.log(user.id);
-    console.log(await Transaction.findOne({userId : user.id}));
+    console.log(await Transaction.findOne({ userId: user.id }));
     // var transaction = {};
     // transaction.id = shortid.generate();
     // transaction.userId = user.id;
@@ -86,7 +86,7 @@ var transactionId =await Transaction.findOne({userId : user.id})
     //   .find({ id: sessionId })
     //   .unset("cart")
     //   .write();
-    
+
     res.redirect("/transactions");
   } else {
     // var addBook = Object.assign(transactionId.book, data.cart);
@@ -98,9 +98,13 @@ var transactionId =await Transaction.findOne({userId : user.id})
     //   .find({ id: sessionId })
     //   .unset("cart")
     //   .write();
-    await Transaction.findOneAndUpdate(user.Id , { booksId: data.cart });
+    await Transaction.findOneAndUpdate
+    (user.Id, { booksId: data.cart },  {
+  new: true,
+  upsert: true // Make this update into an upsert
+});
     await Session.findOneAndUpdate({ sessionId }, { cart: {} });
-    console.log(await Transaction.findOne({userId : user.id}));
+    console.log(await Transaction.findOne({ userId: user.id }));
     res.redirect("/transactions");
   }
 };
