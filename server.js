68e202702c5d17d3ff8b9820663be052527e5204
Glitch (spectacled-grape-icecream) <none>
2020-06-05 15:36:14 +0000

    🚅🌲 Checkpoint ./server.js:15968247/4572

--- server.js
+++ server.js
@@ -1,50 +1,47 @@
-require('dotenv').config();
- 
+require("dotenv").config();
+
 const express = require("express");
 
-const bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser')
-var mongoose = require('mongoose');
-var url ='mongodb+srv://admin:123a123@keochanhnhungkchua-nzm8q.gcp.mongodb.net/book-store?retryWrites=true&w=majority';
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var mongoose = require("mongoose");
+var url =
+  "mongodb+srv://admin:123a123@keochanhnhungkchua-nzm8q.gcp.mongodb.net/book-store?retryWrites=true&w=majority";
 
-
-// mongoose.connect(process.env.MONGO_URI, { 
+// mongoose.connect(process.env.MONGO_URI, {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
-//   useFindAndModify: false  
+//   useFindAndModify: false
 // });
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
 });
-mongoose.connection.on('connected', () => {
-  console.log('connect mongo db'); 
-})
+mongoose.connection.on("connected", () => {
+  console.log("connect mongo db");
+});
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
- 
-
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 //const userRouter = require("./routes/user.router");
 const bookRouter = require("./routes/book.router");
-const transactionRouter =  require("./routes/transaction.router");
+const transactionRouter = require("./routes/transaction.router");
 const authRouter = require("./routes/auth.router");
-const cartRouter = require("./routes/cart.router"); 
+const cartRouter = require("./routes/cart.router");
 
 const apiTransactionRouter = require("./api/routes/transaction.router");
 const apiAuthRouter = require("./api/routes/auth.router");
 const apiBookRouter = require("./api/routes/book.router");
 
-const authMiddleware =  require("./middleware/auth.middleware");
-const sessionIdMiddleware = require('./middleware/session.middleware');
+const authMiddleware = require("./middleware/auth.middleware");
+const sessionIdMiddleware = require("./middleware/session.middleware");
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -53,21 +50,21 @@ app.use(cookieParser("process.env.SECRET_COOKIES"));
 app.use(sessionIdMiddleware);
 
 //home page
-app.get("/" , (req, res)=> {
+app.get("/", (req, res) => {
   res.render("index");
 });
 
 //app.use("/users",authMiddleware.requireAuth,authMiddleware.isAdmin , userRouter);
 app.use("/books", bookRouter);
-app.use("/transactions",authMiddleware.requireAuth , transactionRouter);
+app.use("/transactions", authMiddleware.requireAuth, transactionRouter);
 app.use("/login", authRouter);
 app.use("/cart", cartRouter);
 
 app.use("/api/transactions", apiTransactionRouter);
 app.use("/api/login", apiAuthRouter);
-app.use("/api/book", apiBookRouter);
+app.use("/api/books", apiBookRouter);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
