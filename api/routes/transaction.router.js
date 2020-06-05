b8f6fd26b941d7b853cf43623d323797e22f65e4
Glitch (spectacled-grape-icecream) <none>
2020-06-05 09:16:54 +0000

    🦀🏮 Checkpoint ./api/routes/transaction.router.js:15968247/1 ./api/controllers/transaction.controller.js:15968247/979 ./api/controllers/auth.controller.js:15968247/2016

--- api/routes/transaction.router.js
+++ api/routes/transaction.router.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var controller = require("../controller/transaction.controller");
+var controller = require("../controllers/transaction.controller");
 
 router.get("/", controller.index);
 module.exports = router;
