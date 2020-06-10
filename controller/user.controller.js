75c6cd402ab716f3faa65fbd3692fb26a37b69f3
Glitch (spectacled-grape-icecream) <none>
2020-06-10 08:03:02 +0000

    🙇🐡 Checkpoint ./controller/user.controller.js:15968247/6406

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -15,14 +15,14 @@ module.exports.index = async (req, res) => {
   res.render("users", { users });
 };
 
-module.exports.deleteUser =async (req, res) =>{
-  var id=req.params.id;
+module.exports.deleteUser = async (req, res) => {
+  var id = req.params.id;
   // db.get("users")
   //   .remove({id})
   //   .write();
-   await User.findByIdAndDelete(id)
+  await User.findByIdAndDelete(id);
   res.redirect("back");
-}
+};
 
 module.exports.editUser = async (req, res) => {
   var id = req.params.id;
@@ -32,29 +32,23 @@ module.exports.editUser = async (req, res) => {
 
 //post
 module.exports.postCreateUser = async (req, res) => {
- // req.body.id = shortid.generate();
-  var avatarUrl;
   if (req.file) {
-    cloudinary.uploader.upload(req.file.path, 
-                               { tags: "avatar" },
-                                await function(err,result){
-                                avatarUrl= result.url;
-                                });
-  }
-  req.body.avatar = avatarUrl;
-  console.log(req.body.avatar)
+    cloudinary.uploader.upload(
+      req.file.path,
+      { tags: "avatar" },
+      await function(err, result) {
+        req.body.avatar = result.url;
+        User.create(req.body)
+          .then(user => res.redirect("/users"))
+          .catch(err => console.log(err));
+      }
+    );
+  } else {
     await User.create(req.body, function(err) {
       if (err) return console.log(err);
     });
-    res.redirect("back");
-  // } else {
-  //   // db.get("users")
-  //   //   .push(req.body)
-  //   //   .write();
-  //   await User.create(req.body, function(err) {
-  //     if (err) return console.log(err);
-  //   });
-  //   res.redirect("/users"); }
+    res.redirect("/users");
+  }
 };
 
 module.exports.postEditUser = async (req, res) => {
@@ -65,13 +59,16 @@ module.exports.postEditUser = async (req, res) => {
       result
     ) {
       req.body.avatar = result.url;
+      User.findByIdAndUpdate(id, req.body, { new: true })
+        .then(user => res.redirect("/users"))
+        .catch(err => console.log(err));
     });
-    await User.findByIdAndUpdate(id, {
-      name: req.body.name,
-      email: req.body.email,
-      avatar: req.body.avatar
-    });
-    res.redirect("/users");
+    // await User.findByIdAndUpdate(id, {
+    //   name: req.body.name,
+    //   email: req.body.email,
+    //   avatar: req.body.avatar
+    //});
+   // res.redirect("/users");
   } else {
     await User.findByIdAndUpdate(id, {
       name: req.body.name,
