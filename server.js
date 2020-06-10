6fe7511bb23c0212e80f6fa60aa6ede4534dd1ca
Glitch (seasoned-clever-wednesday) <none>
2020-06-10 14:10:47 +0000

    🌑🐿 Checkpoint ./server.js:15968247/244 ./package.json:15968247/52

--- server.js
+++ server.js
@@ -5,19 +5,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var mongoose = require("mongoose");
-var url =
-  "mongodb+srv://admin:123a123@keochanhnhungkchua-nzm8q.gcp.mongodb.net/book-store?retryWrites=true&w=majority";
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
 });
-// mongoose.connect(url, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false
-// });
+
 mongoose.connection.on("connected", () => {
   console.log("connect mongo db");
 });
