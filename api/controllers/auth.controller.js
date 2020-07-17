af1c2b2414ffef041f5849f0cf65d495221e72c7
Glitch (seasoned-clever-wednesday) <none>
2020-07-17 14:06:14 +0000

    ⛑🏤 Checkpoint ./api/controllers/auth.controller.js:15968247/2690

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,31 +1,29 @@
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 var User = require("../../models/user.model");
 
 const bcrypt = require("bcrypt");
 
-
-
 module.exports.postLogin = async (req, res) => {
-  var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
-  var email = res.body.email;
-  var password = res.body.password;
-  
+  var token = jwt.sign({ foo: "bar" }, "shhhhh");
+  var email = req.body.email;
+  var password = req.body.password;
 
-  // return res.json({
-  //   success: true,
-  //   token:token
-  // });
+//   return res.json({
+//     success: true,
+//     token:token
+//   });
+  
   var user = await User.findOne({ email: email });
   if (!user) {
     const errors = ["User does not exist"];
-    res.status(404).json(errors);
-    return;
+
+    return res.status(404).json(errors);
   }
   //check wrong login
   if (user.wrongLoginCount > 3) {
     const errors = ["User wrong login > 3"];
-    res.status(404).json(errors);
-    return;
+
+    return res.status(404).json(errors);
   }
   //check pass
   var match = await bcrypt.compare(password, user.password);
@@ -33,13 +31,13 @@ module.exports.postLogin = async (req, res) => {
     user.wrongLoginCount = user.wrongLoginCount + 1;
     await user.save();
     const errors = ["Wrong password"];
-    res.status(404).json(errors);
-    return;
+
+    return res.status(404).json(errors);
   }
   user.wrongLoginCount = 0;
   await user.save();
   //create cookie
   res
     .cookie("userId", user.id, { signed: true })
-    .json({ id: user.id, name: user.name, isAdmin: user.isAdmin });
+    .json({ id: user.id, name: user.name, isAdmin: user.isAdmin, avatar:user.avatar });
 };
