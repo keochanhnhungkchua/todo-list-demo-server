963e227c70eb661d285c28434d008b7b90842974
Glitch (seasoned-clever-wednesday) <none>
2020-07-18 09:55:33 +0000

    🎳🐒 Checkpoint ./api/controllers/transaction.controller.js:15968247/198

--- api/controllers/transaction.controller.js
+++ api/controllers/transaction.controller.js
@@ -4,10 +4,14 @@ var Transaction = require("../../models/transaction.model");
 module.exports.index =async (req, res) => {
   // const transactions = await Transaction.find()
   // res.json(transactions)
-  const data = req.header('Authorization')
-  console.log(data);
+  
+  
  return res.json({
     success:'true transactions',
-    data:data
   })
 };
+
+function veryfiToken(req, res, next){
+  
+  const data = req.header('Authorization').slice(6);
+}
