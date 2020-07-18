638b26e10fb3098cc91edac98d9b30c057702135
Glitch (seasoned-clever-wednesday) <none>
2020-07-18 09:44:17 +0000

    👩‍❤️‍💋‍👩🎽 Checkpoint ./api/controllers/transaction.controller.js:15968247/61

--- api/controllers/transaction.controller.js
+++ api/controllers/transaction.controller.js
@@ -4,9 +4,10 @@ var Transaction = require("../../models/transaction.model");
 module.exports.index =async (req, res) => {
   // const transactions = await Transaction.find()
   // res.json(transactions)
-  const data = req.body
+  const data = req.header('Authorization')
+  console.log(data);
  return res.json({
-    success:'true',
+    success:'true transactions',
     data:data
   })
 };
