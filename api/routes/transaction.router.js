90bfcf716346c1fa037c6156571a6a8f19d19e24
Glitch (seasoned-clever-wednesday) <none>
2020-07-18 10:41:47 +0000

    🍨😜 Checkpoint ./api/routes/transaction.router.js:15968247/86

--- api/routes/transaction.router.js
+++ api/routes/transaction.router.js
@@ -8,8 +8,8 @@ var jwt = require('jsonwebtoken');
 
 
 function veryfiToken(req, res, next){
-  const token = req.header('Authorization').slice(7)|| undefined;
-  if(token=== undefined) { return res.status(400).send('Access denied 123')};
+  const token = req.header('Authorization').slice(7);
+  if(!token) { return res.status(400).send('Access denied 123')};
   try{
     const verified = jwt.verify(token, process.env.SECRET_COOKIES)
     process.env.SECRET=verified.key;
