9ee806ad6e89c4102d998bf425b78c30e554a856
Glitch (seasoned-clever-wednesday) <none>
2020-07-20 02:55:27 +0000

    🚟🌥 Checkpoint ./api/controllers/post.controller.js:15968247/1457

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -3,24 +3,23 @@ var jwt = require("jsonwebtoken");
 
 module.exports.postAdd = async (req, res) => {
   const token = req.header("Authorization").slice(7);
+  const userId = jwt.decode(token, { complete: true }).payload.id;
   const text = req.body.text.trim();
-  const decoded = jwt.decode(token, { complete: true });
-
   //const posts = await Post.find();
   //res.json(posts);
 
   return res.json({
-    success: decoded.payload,
+    userId: userId,
     text: text
   });
 };
 
 //get all post
 module.exports.index = async (req, res) => {
-   const posts = await Post.find();
-   res.json(posts );
-//   return res.json({
-//     success: 'decoded.payload'
-    
-//   });
+  const posts = await Post.find({ user: "5ed216e730c4a1ddceaa6d0c" });
+  res.json(posts);
+  //   return res.json({
+  //     success: 'decoded.payload'
+
+  //   });
 };
