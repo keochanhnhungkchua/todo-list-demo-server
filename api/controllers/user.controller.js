54e14f8031042264d67ab4af02c8964364b7d7dc
Glitch (seasoned-clever-wednesday) <none>
2020-07-24 06:47:04 +0000

    🌷🍵 Checkpoint ./api/controllers/user.controller.js:15968247/2966

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,33 +5,28 @@ module.exports.postLike = async (req, res) => {
   const userId = req.body.userId;
   const postId = req.body.postId;
   const post = await Post.findById(postId);
-
-  if (post.like.length === 0) {
+  const { like } = post;
+  if (like.length === 0) {
     post.like = [userId];
     await post.save();
     return res.json({
-      like: post.like,
+ 
       step: " 1 create"
     });
   } else {
-    
     const index = like.indexOf(userId);
     if (index === -1) {
-      like = [...like, userId];
-     // await Post.save();
+      post.like = [...like, userId];
+       await post.save();
       return res.json({
-        index: index,
-        user: userId,
-        like: like,
+      
         step: "2 add"
       });
     } else {
-      like = [...like.slice(0, index), ...like.slice(index + 1)];
-      //await post.save();
+      post.like = [...like.slice(0, index), ...like.slice(index + 1)];
+      await post.save();
       return res.json({
-        index: index,
-        user: userId,
-        like: like,
+  
         step: "3 remove"
       });
     }
