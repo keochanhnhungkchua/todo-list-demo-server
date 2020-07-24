b8180167b1f5219c8d6ab37533829f98df8a3e46
Glitch (seasoned-clever-wednesday) <none>
2020-07-24 03:32:37 +0000

    🏥🎥 Checkpoint ./api/controllers/user.controller.js:15968247/3226

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,33 +5,27 @@ module.exports.postLike = async (req, res) => {
   const userId = req.body.userId;
   const postId = req.body.postId;
   const post = await Post.findById(postId);
-  
+
   if (!post.like.length) {
-    const posts = await Post.findByIdAndUpdate(
-      postId,
-      { $set: { like: [userId] } },
-      { new: true }
-    );
-    return res.json(posts);
+    // const posts = await Post.findByIdAndUpdate(
+    //   postId,
+    //   { $set: { like: [userId] } },
+    //   { new: true }
+    // );
+    post.like=[userId]
+    post.save();
+    return res.json(post);
   } else {
     const like = post.like;
     const index = like.indexOf(userId);
     if (index) {
-      const newLike = [...like.slice(0, index), ...like.slice(index + 1)];
-      const posts = await Post.findByIdAndUpdate(
-        postId,
-        { $set: { like: newLike } },
-        { new: true }
-      );
-      return res.json(posts);
+      post.like = [...like.slice(0, index), ...like.slice(index + 1)];
+      await post.save();
+      return res.json(post);
     } else {
-      const newLike = [...like, userId];
-      const posts = await Post.findByIdAndUpdate(
-        postId,
-        { $set: { like: newLike } },
-        { new: true }
-      );
-      return res.json(posts);
+      post.like = [...like, userId];
+      await post.save();
+      return res.json(post);
     }
   }
   // const newPost = new Post({
