ae801aa1d1c1434f1dabc059d23b8f847dd400a1
Glitch (seasoned-clever-wednesday) <none>
2020-07-24 01:03:01 +0000

    🎏🏚 Checkpoint ./models/post.model.js:15968247/191

--- models/post.model.js
+++ models/post.model.js
@@ -18,6 +18,16 @@ const postSchema = new mongoose.Schema({
     default:
       "https://cdn.glitch.com/1efbf7e2-61ae-47c1-b0b6-c9e2715b61d6%2Fngay-ngat-sac-hoa-anh-dao-sydney.jpg"
   },
+  like: {
+   type: Object,
+    default: {},
+    required: true
+  },
+  comment: {
+   type: Object,
+    default: {},
+    required: true
+  },
   createdAt: {
     type: Date,
     default: Date.now
